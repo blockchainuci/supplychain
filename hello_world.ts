401bbb1d20c0856073e6ba9fd48536dcd69d6f82
Neil Schultz-Cox <neiltcox@icloud.com>
2018-05-21 21:36:53 -0700

    initial

---
+++ hello_world.ts
@@ -0,0 +1,9 @@
+console.log("starting supply chain ...");
+
+import {
+    Account, NetworkType
+} from "nem2-sdk";
+
+const account = Account.generateNewAccount(NetworkType.MIJIN_TEST);
+
+console.log('Your new account address is:', account.address.pretty(), 'and its private key', account.privateKey);
